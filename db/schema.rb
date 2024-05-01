f7b3f86f5a2f8d20da5a48ced4431ddab9d85b62
Sodobi <sodobiv@gmail.com>
2024-05-01 19:44:58 +0300

    scaffold on Values

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_01_161731) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_01_163710) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_01_161731) do
     t.string "name"
     t.string "email"
     t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "values", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "image_id"
+    t.integer "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
